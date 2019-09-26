188c4a9350cbb906f0770b142c2bdacc78bf8173
Alexandru Russu <alexandru.russu@tekoway.com>
2019-09-26 10:47:10 +0300

    2. List of races

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { RacesComponent } from './races/races.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    RacesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/races/races.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import RaceModel from '../models/race.model';
+
+@Component({
+  selector: 'pr-races',
+  templateUrl: './races.component.html',
+  styleUrls: ['./races.component.css']
+})
+export class RacesComponent implements OnInit {
+
+  races: RaceModel[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.races = [{ name: 'Lyon' }, { name: 'London' }];
+  }
+
+}
