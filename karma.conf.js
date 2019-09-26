5abd28f950234450849cccbda3f0a826885d6ca0
Alexandru Russu <alexandru.russu@tekoway.com>
2019-09-26 09:34:30 +0300

    0. Getting started

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,18 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
+    browserNoActivityTimeout: 30000,
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-firefox-launcher'),
+      require('karma-json-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -17,15 +20,30 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/ponyracer'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'json-summary', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    jsonReporter: {
+      stdout: false,
+      outputFile: './results/karma-results.json'
+    },
+    reporters: process.env.CI === 'true' ? ['dots', 'json'] : ['progress', 'json', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: process.env.CI === 'true' ? ['CustomChromeHeadless'] : ['Chrome'],
+    customLaunchers: {
+      CustomChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '-incognito',
+          '--headless',
+          '--remote-debugging-port=9222',
+          '--disable-gpu'
+        ]
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,20 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(() =>
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+      declarations: [AppComponent]
+    })
+  );
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ponyracer'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ponyracer');
-  });
-
-  it('should render title', () => {
+  it('should have a title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ponyracer app is running!');
+    const element = fixture.nativeElement;
+    expect(element.querySelector('h1').textContent)
+      .withContext('You should have an `h1` with the text Ponyracer')
+      .toContain('Ponyracer');
   });
 });
