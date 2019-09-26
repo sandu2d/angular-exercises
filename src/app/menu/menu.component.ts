11541031d9c4a34efd46aeac9b833c7556603717
Alexandru Russu <alexandru.russu@tekoway.com>
2019-09-26 10:29:08 +0300

    1. Templates

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pr-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  public navbarCollapsed = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public toggleNavbar() {
+    this.navbarCollapsed = !this.navbarCollapsed;
+  }
+
+}
